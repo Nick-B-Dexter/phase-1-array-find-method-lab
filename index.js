3f2a3959eadd16082236793498d05380d8a2a908
Nick Dexter <nickdexter23@gmail.com>
2022-08-04 09:28:14 -0500

    commit tree 41d6610d276b18f1073e2905350854f2a4042687

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+const record = [
+    { year: "2022", result: "N/A"},
+    { year: "2021", result: "N/A"},
+    { year: "2020", result: "N/A"},
+    { year: "2019", result: "N/A"},
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "W"},
+    { year: "2015", result: "N/A"},
+    { year: "2014", result: "L"},
+    { year: "2013", result: "N/A"},
+    { year: "2012", result: "N/A"}
+  ]
+
+function superbowlWin(record) {
+    for(let item of record) {
+        if(item.result === "W"){
+            return item.year;
+        }
+    }
+}
